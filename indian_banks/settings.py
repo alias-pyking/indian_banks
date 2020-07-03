798e18567c6f4a2f636330b69ffcbe261ee7252e
shubhamsks <shubhamkumar7051@gmail.com>
2020-07-03 12:31:52 +0530

    heroku configurations

--- indian_banks/settings.py
+++ indian_banks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True 
 CORS_ALLOW_CREDENTIALS = True
+
+django_heroku.settings(locals())
