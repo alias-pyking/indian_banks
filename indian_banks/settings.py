f845b30b201db21bf0fad88fd314efde2e3026ab
shubhamsks <shubhamkumar7051@gmail.com>
2020-07-03 12:39:33 +0530

    deploying to pythonanywhere

--- indian_banks/settings.py
+++ indian_banks/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,5 +128,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True 
 CORS_ALLOW_CREDENTIALS = True
-
-django_heroku.settings(locals())
